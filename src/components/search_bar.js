d5039d243be0db3cce8109cf1c3a3e59f110cd4c
Michael <misankovich@gmail.com>
2016-06-07 22:53:44 -0400

    adding youtube api search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props); //calls the parent method from Component (a lower level class)
+    this.state = { term: '' };
+  }
   render() {
-    return <input />
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}
+        />
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDd1Iqcw5HsouixNOA_ZCsA_bjIs4DbK-s';
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] }
+    YTSearch({key: API_KEY, term: 'Oregairu Preview'}, (videos) => {
+      this.setState({ videos: videos })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
