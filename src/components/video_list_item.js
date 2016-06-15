2d2f6e5cd7b4818a20cda6ed999e6a56d00af04c
Michael <misankovich@gmail.com>
2016-06-14 23:15:41 -0400

    selected video display. play video added

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => { //same as props.video
+const VideoListItem = ({video, onVideoSelect}) => { //same as props.video
   console.log(video)
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className='list-group-item'>
+    <li onClick={()=> onVideoSelect(video)} className='list-group-item'>
       <div className='video-list media'>
         <div className='media-left'>
           <img className='media-object' src={imageUrl}/>
